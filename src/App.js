f6a6debae87615c8f36957531994c2ed6ad4533b
Sean Abbas <sean.abbas33@gmail.com>
2023-03-17 16:15:10 -0400

    phase 1 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import harvardArt from './data/harvardArt'
+import harvardArt from './data/harvardArt';
+import GalleryNavigation from './components/GalleryNavigation';
 
 
 function App() {
-  
   return (
-    <h1>Hello from App</h1>
+    <GalleryNavigation galleries={harvardArt.records} />
   );
 }
 
---
+++ src/components/GalleryNavigation/index.js
@@ -0,0 +1,28 @@
+import {NavLink} from 'react-router-dom';
+import './GalleryNavigation.css';
+
+function GalleryNavigation(props) {
+    let {galleries} = props
+    console.log(galleries)
+    let galleriesArr = []
+
+    for (let i = 0; i < galleries.length; i++) {
+        let link = `/galleries/${galleries[i].id}`
+        galleriesArr.push( 
+        <li key={galleries[i].id}><NavLink to={link}>{galleries[i].name}</NavLink></li>
+        )
+    }
+
+    return (
+        <nav>
+            <NavLink to="/">
+                Home Page
+            </NavLink>
+            <h1>Galleries</h1>
+            <ul>{galleriesArr}</ul>
+        </nav>
+    )
+
+}
+
+export default GalleryNavigation;
